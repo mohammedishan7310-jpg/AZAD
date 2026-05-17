c2712188d44ba9adeadb44c8ca008c496491cb4b
mohammedishan7310-jpg <mohammedishan7310@gmail.com>
2026-05-17 21:41:54 +0530

    Update server.py

--- app-main/backend/server.py
+++ app-main/backend/server.py
@@ -13,7 +13,7 @@ import jwt
 from datetime import datetime, timezone, timedelta
 from typing import List, Optional
 
-from fastapi import FastAPI, APIRouter, HTTPException, Depends, Body
+from fastapi import FastAPI, APIRouter, HTTPException, Depends, Body, File, UploadFile, Form
 from fastapi.responses import Response as FastAPIResponse
 from starlette.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
